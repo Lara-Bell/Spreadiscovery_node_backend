3abc4efc6308d85fad4620756b36dcdc3996a9c5
Lara_Bell <j8cwsqe29e2p@gmail.com>
2018-02-10 18:38:53 +0900

    React Router Dom contact donate privacyPolicy add

--- client_src/src/components/information/donate.js
+++ client_src/src/components/information/donate.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 
+import fontawesome from '@fortawesome/fontawesome';
+import brands from '@fortawesome/fontawesome-free-brands';
+import solid from '@fortawesome/fontawesome-free-solid';
+
+fontawesome.library.add(brands.faBtc);
+fontawesome.library.add(brands.faEthereum);
+fontawesome.library.add(solid.faDollarSign);
+
 class Donate extends Component {
   render() {
     return (
-      <div class="container col-xs-12 content-page donate">
-        <h1><i class="material-icons">thumb_up</i> Donate</h1>
+      <div className="container col-xs-12 content-page donate">
+        <h1 className="mt-5"><i className="fas fa-dollar-sign"></i> Donate</h1>
         <p>Donations help! We'd like to offer real-time API endpoints very soon with more complex conditional evaluation and webhooks.</p>
-        <h2>Bitcoin</h2>
+        <h2 className="mt-5"><i className="fab fa-btc"></i> Bitcoin (BTC)</h2>
         <p>***REMOVED***</p>
-        <h2>Ethereum</h2>
+        <h2 className="mt-5"><i className="fab fa-ethereum"></i> Ethereum (ETH)</h2>
         <p>***REMOVED***</p>
-        <h2>Bitcoin Cash</h2>
+        <h2 className="mt-5">Bitcoin Cash (BTH)</h2>
         <p>***REMOVED***</p>
       </div>
     );
--- client_src/src/components/information/privacyPolicy.js
+++ client_src/src/components/information/privacyPolicy.js
@@ -1,15 +1,28 @@
-Terms Of Use & Privacy Policy
-Don't Be A Jerk
-Don't reproduce our data without permission. This is a free service. If you would like the service to do something it doesn't already, just email us and we can talk.
+import React, { Component } from 'react';
 
-Cookies
-We drop a few cookies to remember things like navigation/filtration options, etc. That's it.
+class Donate extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h1 className="mt-5">Privacy Policy</h1>
 
-Remarketing
-We don't do it. By using this site you are not getting burned with mktg pixels or any such thing — apart from what Google already knows about you, which is a lot.
+        <h2 className="mt-5">What personal information do we collect from the people that visit our blog, website or app?</h2>
+        <p>We do not collect information from visitors of our site.</p>
 
-If You Give Us Your Email:
-We will email you spread alerts. Be careful with your settings as you may get a lot. We recommend that you set your thresholds a bit higher/lower than you might think, as the crypto markets are quite noisy. We also might let you know about exciting things or partnerships we're up to in the future. Right now we're not doing anything exciting except sending simple spread alerts.
+        <h2 className="mt-5">Third-party disclosure</h2>
+        <p>We do not sell, trade, or otherwise transfer to outside parties your Personally Identifiable Information.</p>
 
-What Is The Meaning Of Life?
-"With harmony in the home, the 10,000 things are possible." — Ancient Korean Proverb
+        <h2 className="mt-5">Third-party links</h2>
+        <p>We do not include or offer third-party products or services on our website.</p>
+
+        <h2 className="mt-5">Google</h2>
+        <p>Google's advertising requirements can be summed up by Google's Advertising Principles. They are put in place to provide a positive experience for users.</p>
+        <p>We use Google AdSense on our website.</p>
+
+        <p>This software is a beta version. Please use it at your own risk.</p>
+      </div>
+    );
+  }
+}
+
+export default Donate;
